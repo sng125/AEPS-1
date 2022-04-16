ad3e8a1d86fbea9c440a288d1d08ec4667eaa1e8
RishiSajayUF24 <sajay.rishi@gmail.com>
2022-04-16 14:54:10 -0400

    Added Functioning Gravity Level

---
+++ aeps/Assets/Scripts/GravityLevelScripts/ChangeGreenBallMass.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using TMPro;
+
+public class ChangeGreenBallMass : MonoBehaviour
+{
+
+    float scale = 1;
+    float mass = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<Rigidbody>().mass = scale;
+        //mass = GetComponent<Rigidbody>().mass;
+    }
+
+    public void ChangeMass()
+    {
+        Slider mySlider = GameObject.Find("GreenBallMassSlider").GetComponent<Slider>();
+        float mySliderVal = mySlider.value * 10;
+        mass = mySliderVal;
+
+        Debug.Log(mass);
+
+        //Change mass display
+        //GameObject.Find("GreenBallMassLabel").GetComponent<TMPro.TextMeshProUGUI>().text = "Blue Ball Mass: " + mass;
+
+    }
+    public void UpdateMassLabel()
+    {
+        SpriteRenderer renderer = GameObject.Find("Background").GetComponent<SpriteRenderer>();
+        //renderer.sprite = 
+    }
+}
