50cb72d12f6e9be7f0079907c7ab40c6cbd40d38
sng125 <73617325+sng125@users.noreply.github.com>
2022-04-17 00:59:21 -0400

    Changed Menu System
    Removed the title screen.
    Added working Gravity, Kinematics, and Friction buttons to the level select menu.

--- aeps/Assets/Scripts/LevelSelectScripts/LevelSelectMenu.cs
+++ aeps/Assets/Scripts/LevelSelectScripts/LevelSelectMenu.cs
@@ -5,12 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelectMenu : MonoBehaviour
 {
+    //Button Functions
     public void Back()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
-    public void LoadLevel1()
+    public void LoadGravityLevel()
+    {
+        SceneManager.LoadScene("GravityLevel");
+    }
+
+    public void LoadKinematicsLevel()
+    {
+        SceneManager.LoadScene("KinematicsLevel");
+    }
+
+    public void LoadFrictionLevel()
     {
         SceneManager.LoadScene("FrictionLevel");
     }
